55b15f75b5333e5dad4c6a150a8532557862cafc
ReCpp <gitdiloz@gmail.com>
2020-07-17 15:30:54 +0300

    Добавлена библиотека string

--- Source/ukr-string.h
+++ Source/ukr-string.h
@@ -1,3 +1,83 @@
-#define to_string() в_рядок()
-#define atoi() до_числа()
+//std::string
+
+#define string рядок //Может использоваться как конструктор/деструктор
+/*********************************************************************/
+
+//std::string method`s
+#define to_string в_рядок
+#define atoi до_числа
+#define value_type
+#define traitss_type
+#define allocator_type
+#define reference
+#define const_reference
+#define pointer
+#define const_pointer
+#define iterator
+#define const_iterator
+#define reverse_iterator
+#define const_reverse_iterator
+#define difference_type
+#define size_type розмір_типу
+/*********************************************************************/
+
+//std::string iterators
+#define begin почати
+#define end кінець
+#define rbegin зворотній_початок
+#define rend зворотній_кінець
+#define cbegin зворотній_конст_початок
+#define cend зворотній_конст_кінець
+#define crbegin //?
+#define crend //&
+
+//std::string capacity
+#define size розмір
+#define length довгота
+#define max_size макс_розмір
+#define resize змін_розмір
+#define capacity ємність
+#define reverve резерв
+#define clear очистити
+#define empty порожній
+#define shrink_to_fit //? C++ 11
+/**********************************************************************/
+
+//std::string element`s access
+#define at цей
+#define back назад
+#define front вперед
+
+/**********************************************************************/
+
+//std::string modifiers
+#define append додати
+#define push_back відсунути
+#define assign призначити
+#define insert вставити
+#define erase стерти
+#define replace замінити
+#define swap поміняти_місцями
+#define pop_back видалити_останній_символ
+
+//std::string operations
+#define c_str с_рядок
+#define data дані
+#define get_allocator отримати_розподільник
+#define copy копіювати
+#define find знайти
+#define rfing
+#define find_first_of
+#define find_last_of
+#define find_first_not_of
+#define find_last_not_of
+#define substr суб_рядок
+#define compare порівняти
+
+//std::string member constants
+#define npos макс_розмір_т
+
+
+
+
 
