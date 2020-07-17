cefe41749a9f8f5c4657113ca029843f80493c32
ReCpp <gitdiloz@gmail.com>
2020-07-17 19:57:26 +0300

    Добавлен std::list

---
+++ Source/ukr-list.h
@@ -0,0 +1,43 @@
+//std::list
+//-------------------------------------------------------
+#define list список
+//-------------------------------------------------------
+//
+//Elements access
+#define front перед
+#define back назад
+
+//-------------------------------------------------------
+//
+//Iterators
+#define begin почати
+#define end кінець
+#define rend //?
+
+//Capacity
+#define empty порожній
+#define size розмір
+#define max_size макс_розмір
+
+//Modifiers
+#define clear очистити
+#define insert вставити
+#define emplace //?
+#define erase стерти
+#define push_back відсунути
+#define emplace_back
+#define pop_back відскочити_назад
+#define push_front
+#define emplace_front //?
+#define pop_front
+#define resize змінити_розмір
+#define swap своп
+
+//Operations
+#define merge
+#define splice
+#define remove
+#define remove_if
+#define reverse
+#define unique
+#define sort
