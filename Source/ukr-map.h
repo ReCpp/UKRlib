356f58a5219caca83d8674204adaadac5326d38e
ReCpp <gitdiloz@gmail.com>
2020-07-17 19:57:43 +0300

    Добавлен std::map

---
+++ Source/ukr-map.h
@@ -0,0 +1,44 @@
+//std::map
+//by Divan Lozinskiy
+//---------------------------------------------------
+
+//Member classes
+#define value_compare
+
+//Element access
+#define at цей
+#define operator оператор
+
+//Iterators
+#define begin початок
+#define end кінець
+#define rend //?
+
+//Capacity
+#define empty порожній
+#define size розмір
+#define max_size макс_розмір
+
+//Modifiers
+#define clear очистити
+#define insert втсавити
+#define insert_of_assign //?
+#define emplace //?
+#define emplace_hint //?
+#define try_emplace //?
+#define erase стерти
+#define swap своп
+#define extract витягти
+#define merge перейти
+
+//Lookup
+#define count рахунок
+#define find знайти
+#define contains вміст
+#define equal_range вирівняти_дистанцію
+#define lower_bound нижній_бійний
+#define upper_bound верхній_бійний
+
+//Observers
+#define key_comp //?
+#define value_comp //?
