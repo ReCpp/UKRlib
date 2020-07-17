feed67d72783bb48623a8ce10170151c0f9b6113
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2020-07-17 18:00:32 +0300

    Create ukr-algorithm.h

---
+++ Source/ukr-algorithm.h
@@ -0,0 +1,61 @@
+#pragma once
+//Методы algorithm 
+//by FirstAfterGod2501
+//---------------------------------------------------------------------
+//Не изменяющие последовательные операции
+#define adjacent_find сусідній_визначити
+#define count рахувати
+#define equal рівний
+#define find знайти
+#define for_each для_кожного
+#define mismatch невідповідність
+#define search пошук
+//---------------------------------------------------------------------
+//Изменяющие последовательные операции
+#define fill заливка
+#define generate генерувати
+#define iter_swap iter_своп
+#define remove видалити
+#define reverse зворотний
+#define replace замінити
+#define rotate обертати
+#define swap міняти_місцями
+#define swap_ranges діапазони_змін
+#define transform перетворення
+#define unique унікальний
+//---------------------------------------------------------------------
+//Операции сортировки
+#define nth_element n-й_елемент
+#define sort сортувати
+#define stable_sort стабільний_сорт
+//---------------------------------------------------------------------
+//Бинарные операции поиска
+#define binary_search двійковий_пошук
+#define equal_range рівний_помаранчевий
+#define lower_bound нижній_бійний
+#define upper_bound верхній_бійний
+//---------------------------------------------------------------------
+//Операции слияния
+#define includes включає
+#define merge злиття
+#define set_difference встановити_різницю
+#define set_intersection встановити_перехрестя
+#define set_union встановити_союз //Нерушимый республик свободных
+//---------------------------------------------------------------------
+//Кучи
+#define make_heap зробити_купу
+#define pop_heap поп_купи //маамкупи
+#define push_heap штовхати купу
+#define sort_heap сортувати купу
+//---------------------------------------------------------------------
+//Операции отношений
+#define lexicographical_compare	лексикографічний_порівняти//йобаныйврот какое название
+#define max макс //иди нахуй
+#define max_element макс_елемент 
+#define min мінімум
+#define min_element мінімальний_елемент
+#define next_permutation наступна_перестановка
+#define prev_permutation попередня_перестановка
+
+
+
