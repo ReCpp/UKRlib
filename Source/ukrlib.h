04612aba31124105d7d6bebac6dcc5248404eae2
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2020-07-17 18:14:23 +0300

    Update ukrlib.h

--- Source/ukrlib.h
+++ Source/ukrlib.h
@@ -1,9 +1,15 @@
 /*
  * Эта библиотка включает в себя все библиотки UKRLIB, сделано для удобства
+ Cчитай,включаешь std
  */
 
 #pragma once
 
-#include "datatype.h"
-#include "syntax.h"
-#include "ukr-std.h"
+#include datatype.h
+#include syntax.h
+#include ukr-algorithm.h
+#include ukr-ctime.h
+#include ukr-memory.h
+#include ukr-std.h
+#include ukr-string.h
+#include ukr-vector.h
