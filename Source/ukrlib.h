3555bde852b72362069959651f42c63650933de4
ReCpp <gitdiloz@gmail.com>
2020-07-17 19:23:23 +0300

    Исправлены синтаксические ошибки

--- Source/ukrlib.h
+++ Source/ukrlib.h
@@ -5,11 +5,7 @@
 
 #pragma once
 
-#include datatype.h
-#include syntax.h
-#include ukr-algorithm.h
-#include ukr-ctime.h
-#include ukr-memory.h
-#include ukr-std.h
-#include ukr-string.h
-#include ukr-vector.h
+#include "datatype.h"
+#include "syntax.h"
+#include "ukr-algorithm.h"
+#include "ukr-std.h"
